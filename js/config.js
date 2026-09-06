af171f456fc7f44c4d65860ca03d0ca3ab26dbb4
Alexandr <106959698+AfanasevAlexandr@users.noreply.github.com>
2026-09-06 21:45:36 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,11 +5,11 @@
 export const CONFIG = {
   // URL Web App из Apps Script (файл Code.gs), отдающий JSON книги рецептов.
   // Пример: 'https://script.google.com/macros/s/AKfycb.../exec'
-  CATALOG_API_URL: 'ВСТАВЬТЕ_СЮДА_URL_ВАШЕГО_APPS_SCRIPT_WEB_APP',
+  CATALOG_API_URL: 'https://script.google.com/macros/s/AKfycbyUEouphkK9ER7atGvUmPoK0e5AtoHZ1Vgzg-8mL08HSsQu7Z_YVyTJOqeZXOLoF9sS/exec',
 
   // Сколько миллисекунд хранить книгу рецептов в кэше браузера
   // (localStorage), чтобы не дёргать Apps Script при каждом открытии.
-  CATALOG_CACHE_TTL_MS: 10 * 60 * 1000, // 10 минут
+  CATALOG_CACHE_TTL_MS: 5 * 60 * 1000, // 5 минут
 
   // Название приложения, показывается в шапке.
   APP_TITLE: 'Кулинарная книга',
